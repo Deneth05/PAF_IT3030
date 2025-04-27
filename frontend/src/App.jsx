8dbb8a3e24d1ea0a416791f07d7c2c483bc8cb80
Isu12 <it22294852@my.sliit.lk>
2025-04-27 15:22:57 +0530

    merged-learning progress update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
 import CreatePost from './screens/CreatePost';
 import { CommentsProvider } from './context/CommentContext';  // Import CommentsProvider
-<<<<<<< HEAD
 import Navbar from './components/nav-bar/nav-bar';
 import LearningProgress from './components/learning-progress/learning-progress-form';
 import Footer from './components/footer/footer';
 import LearningProgressList from './components/learning-progress/learning-progress-list';
 import Login from './components/log-in/login';
 import Register from './components/register/register';
-=======
 import LearningPlanForm from './components/Learning-plan/LearningPlanForm';
->>>>>>> feature/learning-plan
 
 function App() {
 
     return (
-<<<<<<< HEAD
         <CommentsProvider>
             <Router>
                 <Navbar />
@@ -27,6 +23,7 @@ function App() {
                     <Route path="/create-post" element={<CreatePost />} />
                     <Route path="/learning-porogress" element={<LearningProgressList/>} />
                     <Route path="/learning-progress/edit/:id" element={<LearningProgress />} />
+                    <Route path="/learning-plan" element={<LearningPlanForm/>} />
 
                     <Route path="/login/" element={<Login />} />
                     <Route path="/register/" element={<Register />} />
@@ -34,17 +31,6 @@ function App() {
                 </Routes>
                 <Footer/>
             </Router>
-=======
-      <CommentsProvider>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/posts/:postId" element={<PostDetails />} /> 
-                <Route path="/create-post" element = {<CreatePost/>}/>
-                <Route path="/learning-plan" element = {<LearningPlanForm />} />
-            </Routes>
-        </Router>
->>>>>>> feature/learning-plan
         </CommentsProvider>
     );
 }
